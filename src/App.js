fe7d4d66d7f81a992a6522f7cbea746855dde283
korana <korayut@gmail.com>
2023-03-18 23:03:18 +0700

    feat: delete Getstart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home"
 import Login from "./pages/Login"
+import GetStart from "./pages/GetStart";
 
 function App() {
   return (
