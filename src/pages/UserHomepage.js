edd1cdd1f1af083d856bcf7e2000263785ebf447
korana <korayut@gmail.com>
2023-03-22 01:35:22 +0700

    feat: add duration of hover for User Homepage

--- src/pages/UserHomepage.js
+++ src/pages/UserHomepage.js
@@ -9,8 +9,8 @@ const UserHomepage = () => {
     <div className='mt-5'>
       <span className='text-2xl mx-5'>KARENTOROKU</span>
       <div className='flex mt-8 space-x-5 p-2 border-b-[2px] border-gray'>
-        <div className='ml-3 hover:font-bold cursor-pointer'>Event Types</div>
-        <div className='hover:font-bold cursor-pointer'>Scheduled Events</div>
+        <div className='ml-3 hover:font-bold cursor-pointer duration-300'>Event Types</div>
+        <div className='hover:font-bold cursor-pointer duration-300'>Scheduled Events</div>
       </div>
       <div className='flex space-x-16 items-center border-b-[2px] border-gray'>
         <div className='text-6xl my-6 ml-5'>
