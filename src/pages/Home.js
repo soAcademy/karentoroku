f061bf88324eaeed5e857784b205cb97f357dddd
korana <korayut@gmail.com>
2023-03-21 12:34:57 +0700

    feat: Add feature/k002 to readme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,9 @@
-import logo from "../logo.svg";
+import React from 'react'
 
-export const Home = () => {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+    <div>Home</div>
+  )
+}
+
+export default Home
