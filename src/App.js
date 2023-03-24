77e46a9ec41054eea27b249dda77fa063ea4bcdd
korana <korayut@gmail.com>
2023-03-24 23:10:59 +0700

    feat: add route in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import GetStart from "./pages/GetStart"
 import UserHomepage from "./pages/UserHomepage";
+import Aboutus from "./pages/Aboutus";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/Login" element={<Login />} />
         <Route exact path="/GetStart" element={<GetStart />} />
         <Route exact path="/UserHomepage" element={<UserHomepage />} />
+        <Route exact path="/Aboutus" element={<Aboutus />} />
       </Routes>
       </>
     </BrowserRouter>
