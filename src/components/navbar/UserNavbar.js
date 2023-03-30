4dbd081436f1e3c7cbf03d97ae880bbd52d7490e
Phisit Siprasatthong <phisite@gmail.com>
2023-03-30 13:41:07 +0700

    feat: add firebase auth-relate code to SetAvailability page and UserNavbar component

--- src/components/navbar/UserNavbar.js
+++ src/components/navbar/UserNavbar.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import Logo from "../img/logocolor.png";
 import { BsPerson } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../auth/config";
 
 const UserNavbar = () => {
   const [open, setOpen] = useState(false);
-  const [availability, setAvailability] = useState(false)
+  const [availability, setAvailability] = useState(false);
 
   const handleOpen = () => {
     setOpen(!open);
@@ -20,8 +22,9 @@ const UserNavbar = () => {
     navigate("/UserHomepage");
   };
 
-  const logOut = () => {
+  const logOut = async () => {
     try {
+      await signOut(auth);
       localStorage.clear();
       window.location.reload();
     } catch (error) {
@@ -46,12 +49,16 @@ const UserNavbar = () => {
           </div>
           <div className="flex md:space-x-5 lg:space-x-8">
             <div className="hidden items-center md:flex md:space-x-5 lg:space-x-8">
-              <div className="text-xl cursor-pointer font-bold" onClick={toHome}>
+              <div
+                className="cursor-pointer text-xl font-bold"
+                onClick={toHome}
+              >
                 Home
               </div>
               <Link
                 to="/setAvailability"
-                className="nav-links text-xl text-gray-400" onClick={setAvailability}
+                className="nav-links text-xl text-gray-400"
+                onClick={setAvailability}
               >
                 Availability
               </Link>
