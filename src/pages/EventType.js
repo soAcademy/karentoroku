728805244adedd92b3f6dd06002bc39a687e1174
korana <korayut@gmail.com>
2023-04-03 18:48:47 +0700

    feat: add code for linking create event Type end point

--- src/pages/EventType.js
+++ src/pages/EventType.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserNavbar from "../components/navbar/UserNavbar";
 import { useNavigate } from "react-router-dom";
 
-const EventType = () => {
 
-  let navigate = useNavigate()
-  const addEvent = () => {
-    navigate('/EventTypeP2')
-  }
+const EventType = () => {
+  const [eventName, setEventName] = useState("Event Name");
+  const [location, setLocation] = useState("No location given");
+  const [description, setDescription] = useState()
+
+  useEffect(() => {
+    localStorage.setItem("EventData", JSON.stringify());
+  }, []);
+
+  let navigate = useNavigate();
+  const submitData = (e) => {
+    e.preventDefault();
+    const data = {
+      EventName: e.target[0].value,
+      Location: e.target[1].value,
+      Description: e.target[2].value,
+    };
+    console.log(data);
+    localStorage.setItem("EventData", JSON.stringify(data));
+    navigate("/EventTypeP2");
+  };
 
   return (
     <>
@@ -17,26 +33,49 @@ const EventType = () => {
       <div className="border-b-[1px] border-black py-5 text-center text-2xl">
         Add New Event Type
       </div>
-      <div className="mt-3 ml-5">
-        <p className="text-base">What event is this?</p>
-        <p className="text-sm text-gray-400">Event name, No location given</p>
-      </div>
-      <div className="mx-5 mt-7 flex flex-col gap-3">
-        <label className="text-lg">Event name</label>
-        <input className="rounded-xl border-2 border-gray-400 bg-white p-3 text-lg" />
-        <label className="text-lg">Location</label>
-        <input className="rounded-xl border-2 border-gray-400 bg-white p-3 text-lg" />
-        <label className="text-lg">Description/Instructions</label>
-        <textarea rows="8" className="rounded-xl border-2 border-gray-400" />
-      </div>
-      <div className=" mt-14 flex flex-col items-center justify-center gap-5 py-5 text-center">
-        <button className="rounded-2xl border-2 bg-orange-700 py-3 px-20 text-lg text-white" onClick={addEvent}>
-          Next
-        </button>
-        <button className="rounded-2xl border-2 py-3 px-20 text-lg">
-          Cancel
-        </button>
-      </div>
+      <form onSubmit={(e) => submitData(e)}>
+        <div className="mt-3 ml-5">
+          <p className="text-base">What event is this?</p>
+          <p className="text-sm text-gray-400">
+            {eventName}, {location}
+          </p>
+        </div>
+        <div className="mx-5 mt-7 flex flex-col gap-3">
+          <label className="text-lg">Event name</label>
+          <input
+            id="eventName"
+            placeholder="Meeting"
+            className="rounded-xl border-2 border-gray-400 bg-white p-3 text-lg"
+            onChange={(e) => setEventName(e.target.value)}
+          />
+          <label className="text-lg">Location</label>
+          <input
+            id="location"
+            placeholder="MS Team, Zoom, Google Meet"
+            className="rounded-xl border-2 border-gray-400 bg-white p-3 text-lg"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <label className="text-lg">Description/Instructions</label>
+          <textarea
+            id="description"
+            placeholder="This is a book"
+            rows="8"
+            className="rounded-xl border-2 border-gray-400"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className=" mt-14 flex flex-col items-center justify-center gap-5 py-5 text-center">
+          <button
+            type="submit"
+            className="rounded-2xl border-2 bg-orange-700 py-3 px-20 text-lg text-white"
+          >
+            Next
+          </button>
+          <button className="rounded-2xl border-2 py-3 px-20 text-lg">
+            Cancel
+          </button>
+        </div>
+      </form>
     </>
   );
 };
--- src/pages/SetAvailability.js
+++ src/pages/SetAvailability.js
@@ -6,6 +6,9 @@ import SetWorkingHours from "../components/SetWorkingHours";
 import SetCustomHours from "../components/SetCustomHours";
 import { useNavigate } from "react-router-dom";
 // import dayjs from "dayjs";
+import { onAuthStateChanged } from "firebase/auth";
+import axios from "axios";
+import { auth } from "../components/auth/config";
 
 const schedules = [
   { id: 1, name: "Working hours" },
@@ -18,8 +21,47 @@ const SetAvailability = () => {
   const [value, setValue] = useState("");
 
   useEffect(() => {
-  setValue(localStorage.getItem("status"));
-},[])
+    setValue(localStorage.getItem("status"));
+  }, []);
+
+  // useEffect(() => {
+  //   onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       // https://www.freecodecamp.org/news/use-firebase-authentication-in-a-react-app/
+  //       // User is signed in, see docs for a list of available properties
+  //       // https://firebase.google.com/docs/reference/js/auth.user
+  //       const uid = user.uid;
+  //       // ...
+  //       // console.log(user);
+  //       console.log("uid", uid);
+  //       auth.currentUser
+  //         .getIdToken(/* forceRefresh */ true)
+  //         .then(function (idToken) {
+  //           // Send token to your backend via HTTPS
+  //           // ...
+  //           // console.log(idToken);
+  //           axios
+  //             .post("http://localhost:8000/getUserByToken", {
+  //               idToken: idToken,
+  //             })
+  //             .then(function (response) {
+  //               console.log(response);
+  //             })
+  //             .catch(function (error) {
+  //               console.log(error);
+  //             });
+  //         })
+  //         .catch(function (error) {
+  //           // Handle error
+  //           console.log(error);
+  //         });
+  //     } else {
+  //       // User is signed out
+  //       // ...
+  //       console.log("user is logged out");
+  //     }
+  //   });
+  // }, []);
 
   let navigate = useNavigate();
   if (!value) {
